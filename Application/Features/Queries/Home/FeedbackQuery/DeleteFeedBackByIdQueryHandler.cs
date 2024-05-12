944c8e9b638deef9850f8bee91e5e2443b1c8d23
Bulat Sharapow <bulat.nureeff@yandex.ru>
2024-05-12 18:27:12 +0300

    WIP : добавил удаление отзывов

---
+++ Application/Features/Queries/Home/FeedbackQuery/DeleteFeedBackByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces;
+using Contracts.Requests.Home.GetIndex;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Queries.Home.FeedbackQuery;
+
+public class DeleteFeedBackByIdQueryHandler(IDbContext context) : IRequestHandler<DeleteFeedbackByIdQuery>
+{
+    public async Task Handle(DeleteFeedbackByIdQuery request, CancellationToken cancellationToken)
+    {
+        var feedBack = await context.FeedbackFeeds.
+            FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken: cancellationToken);
+
+        if (feedBack != null) 
+            context.FeedbackFeeds.Remove(feedBack);
+        
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
--- learnToProgramm/Controllers/FeedbackController.cs
+++ learnToProgramm/Controllers/FeedbackController.cs
@@ -1,3 +1,4 @@
+using Application.Features.Queries.Home.FeedbackQuery;
 using Application.Features.Queries.TeacherAndAdmin;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -8,11 +9,21 @@ namespace learnToProgramm.Controllers;
 [Authorize(Roles = "Teacher, Admin")] // Убедитесь, что только авторизованные пользователи с нужными ролями имеют доступ
 public class FeedbackController(IMediator mediator) : Controller
 {
-        public async Task<IActionResult> Index(CancellationToken cancellationToken)
-        {
-                var query = new GetFeedbackQuery();
-                var response = await mediator.Send(query, cancellationToken);
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var query = new GetFeedbackQuery();
+        var response = await mediator.Send(query, cancellationToken);
 
-                return View(response);
-        }
+        return View(response);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> DeleteFeedbackById(Guid id,
+        CancellationToken cancellationToken)
+    {
+        var query = new DeleteFeedbackByIdQuery(id);
+        await mediator.Send(query, cancellationToken);
+
+        return RedirectToAction("Index");
+    }
 }
