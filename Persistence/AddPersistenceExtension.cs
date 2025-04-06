0fac29db4c677ba4ab8687325089b6fa163ea80d
Bulat Sharapov <BSharapov@kzn.beeline.ru>
2025-04-06 17:25:11 +0300

    добавлена автомиграция

--- Persistence/AddPersistenceExtension.cs
+++ Persistence/AddPersistenceExtension.cs
@@ -8,6 +8,19 @@ namespace Persistence;
 public static class AddPersistenceExtension
 {
     public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
-        => services.AddDbContext<IDbContext, DbContext>(opt =>
+    {
+        services.AddDbContext<IDbContext, DbContext>(opt =>
             opt.UseNpgsql(configuration.GetConnectionString("PostgresConnection")));
+
+        // Только для development-окружения!
+        if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+        {
+            var provider = services.BuildServiceProvider();
+            using var scope = provider.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<DbContext>();
+            db.Database.Migrate();
+        }
+
+        return services;
+    }
 }
