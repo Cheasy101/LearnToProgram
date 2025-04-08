129599d0c18808f733380f0a05adf468db11d7c0
Bulat Sharapov <BSharapov@kzn.beeline.ru>
2025-04-09 01:07:48 +0300

    Обновлена логика mailSender

--- Services/EmailSender/EmailSender.cs
+++ Services/EmailSender/EmailSender.cs
@@ -8,33 +8,26 @@ namespace Services.EmailSender;
 /// <inheritdoc cref="IEmailSender"/>
 public class EmailSender(IConfiguration configuration) : IEmailSender
 {
-    /// <inheritdoc cref="IEmailSender"/>
     public async Task SendEmailAsync(string to, string message, CancellationToken cancellationToken)
     {
         var emailConfiguration = configuration.GetSection("EmailSettings");
-        
-        using var emailMessage = new MimeMessage();
-        emailMessage.From.Add(new MailboxAddress(emailConfiguration["FromName"],
-            emailConfiguration["EmailAddress"]));
 
-        emailMessage.To.Add(new MailboxAddress("", to));
-        
+        using var emailMessage = new MimeMessage();
+        emailMessage.From.Add(new MailboxAddress(emailConfiguration["FromName"], emailConfiguration["EmailAddress"]));
+        emailMessage.To.Add(MailboxAddress.Parse(to));
         emailMessage.Subject = emailConfiguration["FromName"];
-        
+
         var bodyBuilder = new BodyBuilder { HtmlBody = message };
-        
-        var body = bodyBuilder.ToMessageBody();
-        
-        emailMessage.Body = body;  
-        
+        emailMessage.Body = bodyBuilder.ToMessageBody();
+
         using var client = new SmtpClient();
-            
-        await client.ConnectAsync(emailConfiguration["SMTPServerHost"],
-            int.Parse(emailConfiguration["SMTPServerPort"]!), true, cancellationToken);
-        
-        await client.AuthenticateAsync(emailConfiguration["EmailAddress"],
-            emailConfiguration["Password"], cancellationToken);
-        
+
+        var host = emailConfiguration["SMTPServerHost"];
+        var port = int.Parse(emailConfiguration["SMTPServerPort"]!);
+        var useSsl = bool.Parse(emailConfiguration["UseSsl"] ?? "true");
+
+        await client.ConnectAsync(host, port, useSsl, cancellationToken);
+        await client.AuthenticateAsync(emailConfiguration["EmailAddress"], emailConfiguration["Password"], cancellationToken);
         await client.SendAsync(emailMessage, cancellationToken);
         await client.DisconnectAsync(true, cancellationToken);
     }
